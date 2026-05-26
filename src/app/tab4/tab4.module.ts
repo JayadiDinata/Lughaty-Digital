685588316b19023c45bece787abdef7b70366539
Jaysdes <akungameming01@gmail.com>
2026-05-26 19:39:45 +0700

    fix: add CUSTOM_ELEMENTS_SCHEMA to all page modules for Ionic components

--- src/app/tab4/tab4.module.ts
+++ src/app/tab4/tab4.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -15,6 +15,7 @@ import { Tab4Page } from './tab4.page';
     IonicModule,
     Tab4PageRoutingModule
   ],
-  declarations: [Tab4Page]
+  declarations: [Tab4Page],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class Tab4PageModule {}
--- src/app/tab5/tab5.module.ts
+++ src/app/tab5/tab5.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
@@ -7,6 +7,7 @@ import { Tab5PageRoutingModule } from './tab5-routing.module';
 
 @NgModule({
   imports: [IonicModule, CommonModule, FormsModule, Tab5PageRoutingModule],
-  declarations: [Tab5Page]
+  declarations: [Tab5Page],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class Tab5PageModule {}
